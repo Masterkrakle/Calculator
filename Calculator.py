0bfa05aa8481a756590cac825728289f3926c753
Masterkrakle <149614025+Masterkrakle@users.noreply.github.com>
2024-10-28 17:05:17 +0400

    Update Calculator 2.py
    Added power, square and cubic roots

--- Calculator.py
+++ Calculator.py
@@ -1,22 +1,35 @@
 
-from unicodedata import numeric
 
+import math 
+from unicodedata import numeric
+from math import *
 
 Num1 = input("Number: ")
 Factor = input("")
 Num2 = input("Number: ")
-Div = ["/", "//"]
+Div ={"/", "//"}
 Pow1 = ["P", "p"]
 is_number = True 
+Fac = ("+", "-","*",)
+
 if (Factor in Pow1) and (Factor is numeric):
     Num1 = int(Num1)
     Num2 = int(Num2)
 elif (Factor not in Pow1) and (Factor is numeric):
     Num1 = float(Num1)
     Num2 = float(Num2)
+elif (Factor not in Pow1) and (Factor is not numeric) and (Factor == ("root")):
+    Num1 = float(Num1)
+    Num2 = float(Num2)
+elif (Factor in Fac) or (Factor in Div):
+    Num1 = float(Num1)
+    Num2 = float(Num2)
 else: 
     is_number = False
-    print("Syntax Error")
+    print("Syntax Error1")
+
+def cubic_root(Num2):
+    return round(math.exp(math.log(Num2)/3), 6)
 
 def Pow(Num1, Num2):
     result = 1
@@ -25,7 +38,7 @@ def Pow(Num1, Num2):
     return result
 
 if is_number is False:
-    print("Sytnax Error")
+    print("Sytnax Error2")
 elif Factor == "+":
     print("Your Answer is: ", end='\n')
     print(Num1 + Num2)
@@ -37,9 +50,15 @@ elif Factor == "*":
     print(Num1 * Num2)
 elif Factor in Div:
     print("Your Answer is: ", end='\n')
-    print(Num1 // Num2)
+    print(round(Num1 / Num2, 6))
 elif Factor in Pow1:
     print("Your Answer is: ", end='\n')
     print(Pow(Num1, Num2))
+elif (Factor == "root") and (Num1 == 2) :
+    print("Your Answer is: ", end='\n')
+    print(sqrt(Num2))
+elif (Factor == "root") and (Num1 == 3):
+    print("Your Answer is: ", end='\n')
+    print(cubic_root(Num2))
 else:
-    print("Syntax Error ")
+    print("Syntax Error3")
